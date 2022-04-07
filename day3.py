c3fddc5cd51cc03969dbdb01b5d613c328348e09
mjenks <rikku.hotaru@gmail.com>
2022-04-06 21:47:56 -0400

    day 3
    set up with input value

---
+++ day3.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 06 21:45:07 2022
+
+@author: mjenks
+"""
+    
+def solve(puzzle_data):
+    return 0, 0
+
+    
+puzzle_data = '368078'
+solution1, solution2 = solve(puzzle_data)
+
+print(solution1)
+print(solution2)
