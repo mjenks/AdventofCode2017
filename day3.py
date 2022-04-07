164dc20ace77c71e425d1377ab2c05b526e075bd
mjenks <rikku.hotaru@gmail.com>
2022-04-06 22:30:09 -0400

    part 1
    first found the ring the value is on to get the distance out from center
    then found the value on that center line and found the distance to the
    value

--- day3.py
+++ day3.py
@@ -6,10 +6,40 @@ Created on Wed Apr 06 21:45:07 2022
 """
     
 def solve(puzzle_data):
-    return 0, 0
+    #each ring contains number up to (inclusive) the next odd square
+    #first find which ring the number is on
+    i = 1
+    while i*i < puzzle_data:
+        i += 2
+    ring = i // 2
+    #find where on the ring the value is
+    inner = 2*ring-1
+    pos = puzzle_data - inner*inner
+    if pos - 2*ring <= 0: #on right side
+        center_val = 1
+        for j in range(ring):
+            center_val += 2*(4*j) + 1
+    elif pos - 4*ring <= 0: # top
+        center_val = 1
+        for j in range(ring):
+            center_val += 2*(4*j + 1) + 1
+    elif pos - 6*ring <= 0: #left
+        center_val = 1
+        for j in range(ring):
+            center_val += 2*(4*j + 2) + 1
+    else: #bottom
+        center_val = 1
+        for j in range(ring):
+            center_val += 2*(4*j + 3) + 1
+            
+    #difference between the center value and the number is steps need right or left
+    #ring is the steps need to get to center
+    steps = abs(puzzle_data - center_val) + ring
+        
+    return steps, 0
 
     
-puzzle_data = '368078'
+puzzle_data =  368078
 solution1, solution2 = solve(puzzle_data)
 
 print(solution1)
