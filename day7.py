61f45e6b3ac7f1d8f4bf2af87fd5dd07d7ae0f12
mjenks <rikku.hotaru@gmail.com>
2022-04-09 11:25:32 -0400

    part 1
    sort the programs into tiers to find the one at the bottom

--- day7.py
+++ day7.py
@@ -10,6 +10,7 @@ class Prog:
         self.name = name
         self.weight = weight
         self.holding = holding
+        self.depth = -1
 
 def parse(puzzle_input):
     data = []
@@ -18,7 +19,9 @@ def parse(puzzle_input):
         name = line[0]
         weight = int(line[1][1:-1])
         if len(line) > 2:
-            holding = line[3:]
+            holding = []
+            for item in line[3:]:
+                holding.append(item.split(',')[0])
         else:
             holding = []
         program = Prog(name, weight, holding)
@@ -26,7 +29,33 @@ def parse(puzzle_input):
     return data
     
 def solve(puzzle_data):
-    return 0, 0
+    #build the tree in reverse to find bottom
+    i = 0
+    placed = set()
+    while len(placed) != len(puzzle_data):
+        if i == 0:    
+            for p in puzzle_data:
+                if len(p.holding) == 0:
+                    p.depth = i
+                    placed.add(p.name)
+        else:
+            level = []
+            for p in puzzle_data:
+                if len(p.holding) == 0:
+                    continue
+                place = True
+                for q in p.holding:
+                    if q not in placed:
+                        place = False
+                if place and p.name not in placed:
+                    level.append(p.name)
+                    p.depth = i
+            for name in level:
+                placed.add(name)
+            
+        i += 1
+    
+    return level[0], 0
 
 puzzle_path = "input_day7.txt"
 with open(puzzle_path) as f:
