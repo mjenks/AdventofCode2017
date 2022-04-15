e7185626073c19eadb398c9a863034f239815fa2
mjenks <rikku.hotaru@gmail.com>
2022-04-14 22:23:55 -0400

    part 1
    coded in the text example for testing

--- day10.py
+++ day10.py
@@ -10,16 +10,29 @@ def parse(puzzle_input):
     data = [int(x) for x in line]
     return data
     
-def solve(puzzle_data):
-    current_list = range(256)
-    return 0, 0
+def solve(puzzle_data, size):
+    current_list = range(size)
+    skip_size = 0
+    position = 0
+    for length in puzzle_data:
+        new_list = current_list[:]
+        for i in range(length):
+            new_list[(position + i)%size] = current_list[(position + (length - 1) - i)%size]
+        position = (position + length + skip_size)%size
+        skip_size += 1
+        current_list = new_list[:]
+        
+    return current_list[0]*current_list[1], 0
 
 puzzle_path = "input_day10.txt"
 with open(puzzle_path) as f:
     puzzle_input = f.read()
     
+example_data = [3,4,1,5]
+example = solve(example_data, 5)
+print(example)    
 puzzle_data = parse(puzzle_input)
-solution1, solution2 = solve(puzzle_data)
+solution1, solution2 = solve(puzzle_data, 256)
 
 print(solution1)
 print(solution2)
