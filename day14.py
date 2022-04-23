dacb8e61835500600981bc7405b903abc648a3e2
mjenks <rikku.hotaru@gmail.com>
2022-04-22 21:22:29 -0400

    knot hash
    took the code from day 10 that produced the knot hashes and converted it
    to a function

--- day14.py
+++ day14.py
@@ -5,6 +5,40 @@ Created on Fri Apr 22 21:07:30 2022
 @author: mjenks
 """
 
+def knotHash(puzzle_data):
+    size = 256
+    current_list = range(size)
+    skip_size = 0
+    position = 0
+    rounds = 0
+    while rounds < 64:
+        for length in puzzle_data:
+            new_list = current_list[:]
+            for i in range(length):
+                new_list[(position + i)%size] = current_list[(position + (length - 1) - i)%size]
+            position = (position + length + skip_size)%size
+            skip_size += 1
+            current_list = new_list[:]
+        rounds += 1
+        
+    dense_hash = []
+    i = 0
+    while i < 16:
+        val = 0
+        for j in range(16):
+            val = val^current_list[16*i+j]
+        dense_hash.append(hex(val)[2:])
+        i += 1
+        
+    knot_hash = []
+    for s in dense_hash:
+        if len(s) == 2:
+            knot_hash.append(s)
+        else:
+            knot_hash.append('0')
+            knot_hash.append(s)
+        
+    return ''.join(knot_hash)
     
 def solve(puzzle_data):
     return 0, 0
