b04730ce32e72e79d80726b50729b1a2c0ae6a26
mjenks <rikku.hotaru@gmail.com>
2022-04-22 21:14:16 -0400

    day 14
    Set up code and include my input string

---
+++ day14.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 22 21:07:30 2022
+
+@author: mjenks
+"""
+
+    
+def solve(puzzle_data):
+    return 0, 0
+
+    
+puzzle_data = 'nbysizxe'
+solution1, solution2 = solve(puzzle_data)
+
+print(solution1)
+print(solution2)
