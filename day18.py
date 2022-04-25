f6cb82ae01621cd63105fc5f4430060487f69fa1
mjenks <rikku.hotaru@gmail.com>
2022-04-25 13:50:25 -0400

    part 1
    run instructions until a value is recovered

--- day18.py
+++ day18.py
@@ -20,7 +20,66 @@ def parse(puzzle_input):
     return data
     
 def solve(puzzle_data):
-    return 0, 0
+    played = 0
+    first = 0
+    i = 0
+    while i >= 0 and i < len(puzzle_data):
+        inst = puzzle_data[i]
+        if inst[0] == 'snd':
+            try:
+                played = int(inst[1])
+            except:
+                played = reg[inst[1]]
+            i += 1
+        elif inst[0] == 'set':
+            try:
+                y = int(inst[2])
+            except:
+                y = reg[inst[2]]
+            reg[inst[1]] = y
+            i += 1
+        elif inst[0] == 'add':
+            try:
+                y = int(inst[2])
+            except:
+                y = reg[inst[2]]
+            reg[inst[1]] += y
+            i += 1
+        elif inst[0] == 'mul':
+            try:
+                y = int(inst[2])
+            except:
+                y = reg[inst[2]]
+            reg[inst[1]] = reg[inst[1]]*y
+            i += 1
+        elif inst[0] == 'mod':
+            try:
+                y = int(inst[2])
+            except:
+                y = reg[inst[2]]
+            reg[inst[1]] = reg[inst[1]]%y
+            i += 1
+        elif inst[0] == 'rcv':
+            if reg[inst[1]] != 0:
+                first = played
+                break
+            else:
+                i += 1
+        elif inst[0] == 'jgz':
+            try:
+                x = int(inst[1])
+            except:
+                x = reg[inst[1]]
+            try:
+                y = int(inst[2])
+            except:
+                y = reg[inst[2]]
+            if x > 0:
+                i += y
+            else:
+                i += 1
+                
+    return first, 0
 
 puzzle_path = "input_day18.txt"
 with open(puzzle_path) as f:
