c353c999480301034dc24570429781c73d63a33f
mjenks <rikku.hotaru@gmail.com>
2022-04-25 21:51:30 -0400

    part 1
    followed the path and stored letters as they were crossed

--- day19.py
+++ day19.py
@@ -13,7 +13,74 @@ def parse(puzzle_input): #input is square
     return data
     
 def solve(puzzle_data):
-    return 0, 0
+    letters = []
+    i = 0
+    j = puzzle_data[i].index('|')
+    direction = 'd'
+    end = False
+    while not end:
+        symb = puzzle_data[i][j]
+        if symb == ' ':
+            end = True
+        elif symb == '|':
+            if direction == 'd':
+                s = symb
+                while s != ' ':
+                    i += 1
+                    s = puzzle_data[i][j]
+                    if s.isalpha():
+                        letters.append(s)  
+                i -= 1
+            else:
+                s = symb
+                while s != ' ':
+                    i -= 1
+                    s = puzzle_data[i][j]
+                    if s.isalpha():
+                        letters.append(s) 
+                i += 1
+        elif symb == '-':
+            if direction == 'r':
+                s = symb
+                while s != ' ':
+                    j += 1
+                    s = puzzle_data[i][j]
+                    if s.isalpha():
+                        letters.append(s) 
+                j -= 1
+            else:
+                s = symb
+                while s != ' ':
+                    j -= 1
+                    s = puzzle_data[i][j]
+                    if s.isalpha():
+                        letters.append(s)
+                j += 1
+        elif symb == '+':
+            if direction == 'd' or direction == 'u':
+                if puzzle_data[i][j+1] != ' ':
+                    j += 1
+                    direction = 'r'
+                else:
+                    j -= 1
+                    direction = 'l'
+            else:
+                if puzzle_data[i+1][j] != ' ':
+                    i += 1
+                    direction = 'd'
+                else:
+                    i -= 1
+                    direction = 'u'
+        else: #will happen at the end
+            if direction == 'd':
+                i += 1
+            elif direction == 'u':
+                i -= 1
+            elif direction == 'r':
+                j += 1
+            else:
+                j -= 1
+    return ''.join(letters), 0
 
 puzzle_path = "input_day19.txt"
 with open(puzzle_path) as f:
