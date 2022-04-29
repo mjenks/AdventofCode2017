b9d7fbf998bc834e00fc66efcaacc2820b309726
mjenks <rikku.hotaru@gmail.com>
2022-04-29 15:32:12 -0400

    part 1
    slow because the code finds all possible bridges that can be made and
    their strengths.

--- day24.py
+++ day24.py
@@ -13,7 +13,32 @@ def parse(puzzle_input):
     return data
     
 def solve(puzzle_data):
-    return 0, 0
+    wall = 0
+    comp_sort = [[] for x in range(51)]
+    for i in range(len(puzzle_data)):
+        for pin in puzzle_data[i]:
+            comp_sort[pin].append(i)
+    options = [[[(x, wall)] for x in comp_sort[wall]]]
+    length = 1
+    strength = []
+    while length < len(puzzle_data):
+        new = []
+        for bridge in options[length-1]:
+            comp, used_pin = bridge[-1]
+            comp_used = [x[0] for x in bridge]
+            strength.append(sum([sum(puzzle_data[x]) for x in comp_used]))
+            if puzzle_data[comp][0] != puzzle_data[comp][1]:
+                pin = (set(puzzle_data[comp]) - {used_pin}).pop()
+            else:
+                pin = puzzle_data[comp][0]
+            for new_comp in comp_sort[pin]:
+                if new_comp not in comp_used:
+                    current = bridge[:]
+                    current.append((new_comp, pin))
+                    new.append(current)
+        options.append(new)
+        length += 1
+    return max(strength), 0
 
 puzzle_path = "input_day24.txt"
 with open(puzzle_path) as f:
